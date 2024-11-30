7827b59825bd3e46598d5dcd81353693605651bd
AnjaliSingh2025 <anjalisinghtumaull@gmail.com>
2024-11-30 23:55:07 +0530

    practicejs

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log(typeof(null));
+console.log(typeof(0));
+console.log(typeof(undefined));
+console.log(null==0);
---
+++ typeconversion.js
@@ -0,0 +1,14 @@
+let score="33nsjha";
+console.log(score);
+let valuenumber=Number(score);
+console.log(valuenumber);
+
+// test
+let value=-score;
+console.log(value);
+console.log("2"+2);
+console.log(+true);
+console.log(+"")
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);
